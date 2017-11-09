7263d39c6fe276634c6bab0bbe408df23c4443c1
Patrick <patrick.oconnor@mail.utoronto.ca>
2017-11-09 18:55:38 -0500

    oops forgot to include brushstrategyfactory in the previous commits

---
+++ Assignment2/src/ca/utoronto/utm/paint/BrushStrategyFactory.java
@@ -0,0 +1,54 @@
+package ca.utoronto.utm.paint;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import ca.utoronto.utm.paint.BrushStrategies.BrushStrategy;
+import ca.utoronto.utm.paint.BrushStrategies.CircleBrushStrategy;
+import ca.utoronto.utm.paint.BrushStrategies.PolylineBrushStrategy;
+import ca.utoronto.utm.paint.BrushStrategies.RectangleBrushStrategy;
+import ca.utoronto.utm.paint.BrushStrategies.SquareBrushStrategy;
+import ca.utoronto.utm.paint.BrushStrategies.SquiggleBrushStrategy;
+
+public class BrushStrategyFactory implements ActionListener{
+	private PalletePanel panel;
+	private PaintModel model;
+	private BrushStrategy brushStrategy;
+	
+	public BrushStrategyFactory(PalletePanel panel, PaintModel model){
+		this.panel = panel;
+		this.model = model;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch(e.getActionCommand()){
+		case "Circle":
+			this.brushStrategy = new CircleBrushStrategy(model);
+			break;
+			
+		case "Squiggle":
+			this.brushStrategy = new SquiggleBrushStrategy(model);
+			break;
+			
+		case "Rectangle":
+			this.brushStrategy = new RectangleBrushStrategy(model);
+			break;
+			
+		case "Polyline":
+			this.brushStrategy = new PolylineBrushStrategy(model);
+			break;
+			
+		case "Square":
+			this.brushStrategy = new SquareBrushStrategy(model);
+			break;
+		}
+		JButton button = (JButton)e.getSource();
+		this.panel.toggleButton(this.panel.getSelectedBrushButton(), button);
+		this.panel.setSelectedBrushButton(button);
+		this.panel.setBrushStrategy(brushStrategy);
+	}
+
+}
