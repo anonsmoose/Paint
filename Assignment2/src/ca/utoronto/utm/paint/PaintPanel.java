60adb62191516b7f30f41dae8cf6ac66f1e7196f
Patrick <patrick.oconnor@mail.utoronto.ca>
2017-11-09 19:32:45 -0500

    Cleaned up existing code, bug is fixed

--- Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
+++ Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
@@ -21,7 +21,7 @@ import java.util.Observer;
 // https://docs.oracle.com/javase/8/docs/api/java/awt/Graphics2D.html
 // https://docs.oracle.com/javase/tutorial/2d/
 
-class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseListener  {
+class PaintPanel extends JPanel implements Observer{
 	private PaintModel model; // slight departure from MVC, because of the way painting works
 	private View view; // So we can talk to our parent or other components of the view
 	private BrushStrategy brushStrategy;
@@ -29,38 +29,24 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 	public PaintPanel(PaintModel model, View view){
 		this.setBackground(new Color(43, 43, 43));
 		this.setPreferredSize(new Dimension(600,600));
-		this.addMouseListener(this);
-		this.addMouseMotionListener(this);
+
 		this.model = model;
 		this.model.addObserver(this);
 		this.brushStrategy = new CircleBrushStrategy(this.model);
+		this.addMouseListener(brushStrategy);
+		this.addMouseMotionListener(brushStrategy);
 		this.view=view;
 	}
 
-	/**
-	 *  View aspect of this
-	 */
 	public void paintComponent(Graphics g) {
-		// Use g to draw on the JPanel, lookup java.awt.Graphics in
-		// the javadoc to see more of what this can do for you!!
-		
         super.paintComponent(g); //paint background 
         Graphics2D g2d = (Graphics2D) g; // lets use the advanced api
-		// setBackground (Color.blue); 
-		// Origin is at the top left of the window 50 over, 75 down
-		//int strokeSize = this.view.getPalletePanel().getBrushSize();
-        //g2d.setStroke(new BasicStroke(strokeSize));
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setColor(Color.black);
-        //g2d.drawString ("i="+i, 50, 75);
-		//i=i+1;
-		
-		//draw shapes
+	    //draw shapes
 		for(Shape shape: this.model.getShapes()){
-				shape.drawShape(g);
-				
+				shape.drawShape(g);	
 		}
-		
 		g2d.dispose();
 	}
 
@@ -70,49 +56,16 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 	}
 	
 	public void updateBrushStrategy(BrushStrategy brushStrategy){
+		//Unhook old brush strategy
+		this.removeMouseListener(this.brushStrategy);
+		this.removeMouseMotionListener(this.brushStrategy);
+		
+		//Hook up new brush strategy
 		this.brushStrategy = brushStrategy;
-	}
-	
-	/**
-	 *  Controller aspect of this
-	 */
-
-	// MouseMotionListener below
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		brushStrategy.mouseMoved(e);
-	}
-	
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		brushStrategy.mouseDragged(e);
-		this.updateUI();
-	}
-
-	// MouseListener below
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		brushStrategy.mouseClicked(e);
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		brushStrategy.mousePressed(e);
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		brushStrategy.mouseReleased(e);
-	}
-	
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		brushStrategy.mouseEntered(e);
-	}
-	
-	@Override
-	public void mouseExited(MouseEvent e) {
-		brushStrategy.mouseExited(e);
+		this.addMouseListener(this.brushStrategy);
+		this.addMouseMotionListener(this.brushStrategy);
 	}
 }
+	
+
+
